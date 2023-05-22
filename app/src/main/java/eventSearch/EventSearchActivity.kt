4f0d8476d4e5d33479eeea7ab8f5017cecfab12b
Mango T14 <kubajaskow@wp.pl>
2023-05-22 18:38:43 +0200

    Finish RecyclerAdapter and add mock data to EventSearchActivity.events

--- app/src/main/java/eventSearch/EventSearchActivity.kt
+++ app/src/main/java/eventSearch/EventSearchActivity.kt
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.piastcity.R
+import event.Event
+import event.EventType
+import java.time.LocalDateTime
 import event.Event as PartyEvent
 
-class EventSearchActivity : AppCompatActivity() {
+class EventSearchActivity : AppCompatActivity(), EventSearchRecyclerAdapter.OnItemListener {
     private lateinit var recyclerView: RecyclerView
-    private lateinit var recyclerAdapter: EventSearchRecyclerAdapter
+//    private lateinit var recyclerAdapter: EventSearchRecyclerAdapter
     private lateinit var eventList: ArrayList<PartyEvent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,42 @@ class EventSearchActivity : AppCompatActivity() {
 
         recyclerView = findViewById(R.id.eventRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerView.adapter = EventSearchRecyclerAdapter()
+        setSearchView()
+    }
+
+    private fun setSearchView() {
+        val party1 = PartyEvent("chuj",
+            "dziekanowi",
+            "naryj",
+            EventType.Klub,
+            true,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
+        val party2 = PartyEvent("chuj",
+            "dziekanowi",
+            "naryj",
+            EventType.Klub,
+            true,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
+        val party3 = PartyEvent("chuj",
+            "dziekanowi",
+            "naryj",
+            EventType.Klub,
+            true,
+            LocalDateTime.now(),
+            LocalDateTime.now()
+        )
+        eventList = ArrayList()
+        eventList.add(party1)
+        eventList.add(party2)
+        eventList.add(party3)
+        recyclerView.adapter = EventSearchRecyclerAdapter(eventList, this)
+    }
+
+    override fun onItemClick(position: Int, event: Event) {
+        // open the event activity 
     }
 }
--- app/src/main/java/eventSearch/EventSearchRecyclerAdapter.kt
+++ app/src/main/java/eventSearch/EventSearchRecyclerAdapter.kt
@@ -1,48 +1,68 @@
 package eventSearch
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.piastcity.R
 import org.w3c.dom.Text
 import event.Event as PartyEvent
 
 class EventSearchRecyclerAdapter(
-    private val events: ArrayList<PartyEvent?>,
+    private val events: ArrayList<PartyEvent>,
     private val onItemListener: OnItemListener
 ) : RecyclerView.Adapter<EventSearchRecyclerAdapter.EventSearchViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventSearchViewHolder {
-        TODO("Not yet implemented")
+        return EventSearchViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.event_cell, parent, false),
+            onItemListener,
+            events
+        )
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return events.size
     }
 
     override fun onBindViewHolder(holder: EventSearchViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val event = events[position]
+        holder.eventName.text = "Name: " + event.name
+        holder.eventAddress.text = "Address: " + event.address
+        holder.eventOwner.text = "Owner: " + event.owner
+        holder.eventType.text = "Type: " + event.type.name
     }
 
     interface OnItemListener{
-        fun onItemClick(position: Int)
+        fun onItemClick(position: Int, event: PartyEvent)
     }
 
     class EventSearchViewHolder(
         itemView: View,
         onItemListener: OnItemListener,
-        events: ArrayList<PartyEvent?>
+        events: ArrayList<PartyEvent>
     ): RecyclerView.ViewHolder(itemView), View.OnClickListener {
         private val onItemListener: OnItemListener
-        private val events: ArrayList<PartyEvent?>
-        val parentView: View
+        private val events: ArrayList<PartyEvent>
         val eventName: TextView
+        val eventOwner: TextView
+        val eventAddress: TextView
+        val eventType: TextView
         init {
             this.events = events
+            this.onItemListener = onItemListener
+            itemView.setOnClickListener(this)
+
+            //visible content
+            eventName = itemView.findViewById(R.id.eventName)
+            eventOwner = itemView.findViewById(R.id.eventOwner)
+            eventAddress = itemView.findViewById(R.id.eventAddress)
+            eventType = itemView.findViewById(R.id.eventType)
         }
 
         override fun onClick(p0: View?) {
-            TODO("Not yet implemented")
+            onItemListener.onItemClick(adapterPosition, events[adapterPosition])
         }
 
     }
