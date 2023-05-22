d87cd7e1552bfa9e5e98a31e19b55ecd8dc74faf
Mango T14 <kubajaskow@wp.pl>
2023-05-22 15:04:59 +0200

    Add activity_event_serach.xml and event_cell.xml

--- app/src/main/java/eventSearch/EventSearchActivity.kt
+++ app/src/main/java/eventSearch/EventSearchActivity.kt
@@ -1,4 +1,22 @@
 package eventSearch
 
-class EventSearchActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.piastcity.R
+import event.Event as PartyEvent
+
+class EventSearchActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var recyclerAdapter: EventSearchRecyclerAdapter
+    private lateinit var eventList: ArrayList<PartyEvent>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_search)
+
+        recyclerView = findViewById(R.id.eventRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recyclerView.adapter = EventSearchRecyclerAdapter()
+    }
 }
--- app/src/main/java/eventSearch/EventSearchRecyclerAdapter.kt
+++ app/src/main/java/eventSearch/EventSearchRecyclerAdapter.kt
@@ -1,4 +1,49 @@
 package eventSearch
 
-class EventSearchRecyclerAdapter {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
+import event.Event as PartyEvent
+
+class EventSearchRecyclerAdapter(
+    private val events: ArrayList<PartyEvent?>,
+    private val onItemListener: OnItemListener
+) : RecyclerView.Adapter<EventSearchRecyclerAdapter.EventSearchViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventSearchViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: EventSearchViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    interface OnItemListener{
+        fun onItemClick(position: Int)
+    }
+
+    class EventSearchViewHolder(
+        itemView: View,
+        onItemListener: OnItemListener,
+        events: ArrayList<PartyEvent?>
+    ): RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val onItemListener: OnItemListener
+        private val events: ArrayList<PartyEvent?>
+        val parentView: View
+        val eventName: TextView
+        init {
+            this.events = events
+        }
+
+        override fun onClick(p0: View?) {
+            TODO("Not yet implemented")
+        }
+
+    }
 }
