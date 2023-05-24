56a93187d70fc9a84aa8fd13e2569b94a815dec7
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2023-05-24 17:11:38 +0200

    Add isPublic field to Event class

--- app/src/main/java/event/Event.kt
+++ app/src/main/java/event/Event.kt
@@ -10,11 +10,13 @@ data class Event(
     val type: EventType? = null,
     @field:JvmField
     val isBooze: Boolean? = null,
+    @field:JvmField
+    val isPublic: Boolean? = null,
 //    // x coordinate
 //    val longitude: Double? = null,
 //    // y coordinate
 //    val latitude: Double? = null,
     val creation: Timestamp? = null,
     val start: Timestamp? = null,
-    val end: Timestamp? = null
+    val end: Timestamp? = null,
 )
