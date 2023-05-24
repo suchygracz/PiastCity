3d7717ccec7e024f6dc1d91d0908fd4db1f75975
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2023-05-24 16:51:25 +0200

    Change Event class definition and optimize imports

--- app/src/main/java/com/example/piastcity/AppActivity.kt
+++ app/src/main/java/com/example/piastcity/AppActivity.kt
@@ -1,7 +1,7 @@
 package com.example.piastcity
 // klasa dla aplikacji
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/piastcity/LoginActivity.kt
+++ app/src/main/java/com/example/piastcity/LoginActivity.kt
@@ -1,13 +1,10 @@
 package com.example.piastcity
 // login screen
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.piastcity.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 import eventSearch.EventSearchActivity
