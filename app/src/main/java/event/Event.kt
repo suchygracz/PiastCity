6896f35f8f3cd6eacc8644d4bda28f4c12d5e11e
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2023-05-24 15:55:12 +0200

    Implement database into recycler view - WIP

--- app/src/main/java/event/Event.kt
+++ app/src/main/java/event/Event.kt
@@ -1,13 +1,16 @@
 package event
 
-import java.time.LocalDateTime
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.Exclude
+
 
 data class Event(
-    val name: String,
-    val owner: String,
-    val address: String,
-    val type: EventType,
-    val isBooze: Boolean,
-    val startDT: LocalDateTime,
-    val endDT: LocalDateTime
+    val name: String? = null,
+    val owner: String? = null,
+    val address: String? = null
+//    val type: EventType? = null,
+//    @field:JvmField
+//    val isBooze: Boolean? = null
+//    val startDT: Timestamp? = null,
+//    val endDT: Timestamp? = null
 )
