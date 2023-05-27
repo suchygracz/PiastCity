47d77e701c7b4c1bb0f55df8da4ef1d28a90c000
Wiktor <64496636+suchygracz@users.noreply.github.com>
2023-05-27 17:43:29 +0200

    podpiecie guziczka

--- app/src/main/java/com/example/piastcity/UI.kt
+++ app/src/main/java/com/example/piastcity/UI.kt
@@ -2,10 +2,13 @@ package com.example.piastcity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class UI : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui)
     }
+
+    fun goToMaps(view: View) {}
 }
