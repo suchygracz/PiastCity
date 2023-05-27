c0d36ab65fcd01499d5fc03770e395c213a850b9
Wiktor <64496636+suchygracz@users.noreply.github.com>
2023-05-27 20:49:26 +0200

    dodanie aktywnosci tworzenia użytkownika ale coś sie wypierdala i nie chce po rejestracji tam przejsc

--- app/src/main/java/com/example/piastcity/UI.kt
+++ app/src/main/java/com/example/piastcity/UI.kt
@@ -3,12 +3,18 @@ package com.example.piastcity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class UI : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ui)
+        val user = Firebase.auth.currentUser.toString()
+        Toast.makeText(this, user, Toast.LENGTH_LONG ).show()
     }
+
 
     fun goToMaps(view: View) {}
 }
