188cbbb088fe1abffbfb0f4eef2c274d55a569d3
KiwiGrenade <35775359+KiwiGrenade@users.noreply.github.com>
2023-05-28 15:28:45 +0200

    Add refresh button to recycler layout

--- app/src/main/java/eventSearch/EventSearchActivity.kt
+++ app/src/main/java/eventSearch/EventSearchActivity.kt
@@ -2,44 +2,34 @@ package eventSearch
 
 import android.content.Intent
 import android.os.Bundle
+<<<<<<< HEAD
 import android.view.OrientationEventListener
+=======
+import android.widget.Button
+>>>>>>> b153fdf (Add refresh button to recycler layout)
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.example.piastcity.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import event.Event
 import eventCreation.EventCreator
-import www.sanju.zoomrecyclerlayout.ZoomRecyclerLayout
 import event.Event as PartyEvent
 
 class EventSearchActivity : AppCompatActivity(), EventSearchRecyclerAdapter.OnItemListener {
     private val database = Firebase.firestore
     private lateinit var addEventButton: FloatingActionButton
+    private lateinit var refreshButton: FloatingActionButton
     private lateinit var recyclerView: RecyclerView
     private lateinit var eventList: ArrayList<PartyEvent>
     private var orientationEventListener: OrientationEventListener? = null
     // val owner = FirebaseAuth.getInstance().currentUser!!.displayName
 //    private val owner = FirebaseAuth.getInstance().currentUser!!.uid
     private val owner = "fryta"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_event_search)
-        // attach RecyclerView
-        recyclerView = findViewById(R.id.eventRecyclerView)
-        // set custom recycler layout from github
-        setRecycler()
-        // set button
-        setAddEventOnClickListener()
-    }
-
     override fun onResume() {
         super.onResume()
         setSearchView()
@@ -63,6 +53,17 @@ class EventSearchActivity : AppCompatActivity(), EventSearchRecyclerAdapter.OnIt
 
         orientationEventListener?.disable()
     }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_search)
+
+        // attach RecyclerView
+        recyclerView = findViewById(R.id.eventRecyclerView)
+
+        setRecycler()
+        setButtons()
+    }
+
 //val linearLayoutManager = ZoomRecyclerLayout(this)
     private fun setRecycler() {
         val linearLayoutManager = LinearLayoutManager(this)
@@ -77,10 +78,23 @@ class EventSearchActivity : AppCompatActivity(), EventSearchRecyclerAdapter.OnIt
 
     }
 
+    private fun setButtons() {
+        addEventButton = findViewById(R.id.addEventButton)
+        refreshButton = findViewById(R.id.refreshButton)
+        addEventButton.setOnClickListener {
+            val goToEventCreator = Intent(this, EventCreator::class.java)
+            startActivity(goToEventCreator)
+            setSearchView()
+        }
+        refreshButton.setOnClickListener{
+            setSearchView()
+        }
+    }
+
     private fun setSearchView() {
         eventList = ArrayList()
-        database.collection("events").get().addOnSuccessListener {
-            if(it.isEmpty == false) {
+        database.collection("events").orderBy("creation").get().addOnSuccessListener {
+            if(!it.isEmpty) {
                 for (data in it.documents) {
                     val event = data.toObject<PartyEvent>()
                     eventList.add(event!!)
@@ -90,29 +104,14 @@ class EventSearchActivity : AppCompatActivity(), EventSearchRecyclerAdapter.OnIt
         }
     }
 
-    private fun setAddEventOnClickListener() {
-        addEventButton = findViewById(R.id.addEventButton)
-        addEventButton.setOnClickListener {
-            val goToEventCreator = Intent(this, EventCreator::class.java)
-            startActivity(goToEventCreator)
-        }
-    }
-
     override fun onItemClick(position: Int, event: Event) {
         // like the event
     }
 
     override fun onItemLongClick(position: Int, event: Event) {
-//        && removeImageFromDatabase(event)
-//        if(removeEventFromDatabase(event)) {
             removeEventFromDatabase(event)
             eventList.removeAt(position)
             recyclerView.adapter!!.notifyItemRemoved(position)
-        //            Toast.makeText(this, "Delete successfully!", Toast.LENGTH_LONG).show()
-//        }
-//        else {
-//            Toast.makeText(this, "Delete failure!", Toast.LENGTH_SHORT).show()
-//        }
     }
 
 //    private fun removeImageFromDatabase(event: Event):Boolean {
